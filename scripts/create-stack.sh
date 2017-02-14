244b25503aa8548b9612084d5954f856ca0250b0
Ron Kurr <kurr@kurron.org>
2017-02-14 14:34:30 -0500

    Use stronger types, including a list of subnets instead of individual subnets

--- scripts/create-stack.sh
+++ scripts/create-stack.sh
@@ -4,11 +4,13 @@
 
 STACKNAME=${1:-Weapon-X-ECS}
 PROJECTNAME=${2:-Weapon-X}
-INSTANCETYPE=${3:-m4.large}
-SPOTPRICE=${4:-0.025}
-ENVIRONMENT=${5:-development}
-CREATOR=${6:-CloudFormation}
-TEMPLATELOCATION=${7:-file://$(pwd)/ecs.yml}
+SECURITYGROUPS=${3:-sg-2d0d7955}
+SUBNETS=${4:-subnet-ac62a8e5,subnet-1ae3507d,subnet-af62a8e6,subnet-1be3507c}
+INSTANCETYPE=${5:-m4.large}
+SPOTPRICE=${6:-0.025}
+ENVIRONMENT=${7:-development}
+CREATOR=${8:-CloudFormation}
+TEMPLATELOCATION=${9:-file://$(pwd)/ecs.yml}
 
 VALIDATE="aws cloudformation validate-template --template-body $TEMPLATELOCATION"
 echo $VALIDATE
@@ -22,6 +24,8 @@ CREATE="aws cloudformation create-stack --stack-name $STACKNAME \
                                                      ParameterKey=Creator,ParameterValue=$CREATOR \
                                                      ParameterKey=InstanceType,ParameterValue=$INSTANCETYPE \
                                                      ParameterKey=SpotPrice,ParameterValue=$SPOTPRICE \
+                                                     ParameterKey=Subnets,ParameterValue=\"$SUBNETS\" \
+                                                     ParameterKey=SecurityGroups,ParameterValue=\"$SECURITYGROUPS\" \
                                         --tags Key=Project,Value=$PROJECTNAME \
                                                Key=Environment,Value=$ENVIRONMENT \
                                                Key=Creator,Value=$CREATOR"
